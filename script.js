ccaa4f9a67e2963d816e1882096791f69638dca7
TechyFame <hello@techyfame.com>
2023-04-27 17:17:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showHide(){
+    let nav = document.getElementById('unlist')
+    if(nav.style.display == 'none'){
+        nav.style.display = 'block'
+    }
+    else{
+        nav.style.display = 'none'
+    }
+}
